9fd1a05b496d9c32cf04150bfcd5d5197c214818
Vh Long <vhlong@Vhs-MacBook-Air.local>
2020-06-08 14:03:08 +0700

    {firstReact}

---
+++ script.js
@@ -0,0 +1,16 @@
+const todos = 
+[  'Go to market',
+   'Buy food',
+   'Make dinner'
+];
+// console.log(todos);
+
+
+const li1=React.createElement('li',null,todos[0]);
+const li2=React.createElement('li',null,todos[1]);
+const li3=React.createElement('li',null,todos[2]);
+
+const ul=React.createElement('ul',null,[li1,li2,li3]);
+
+const root=document.getElementById('root');
+ReactDOM.render(ul,root);
